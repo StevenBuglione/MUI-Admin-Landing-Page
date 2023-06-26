1c7291b109176dd8be2a3344886aaede9445c7f5
Steven <stevenbuglion1@gmail.com>
2023-06-26 16:34:16 -0400

    Hidding Logo when smaller

--- src/@core/layouts/components/vertical/appBar/index.tsx
+++ src/@core/layouts/components/vertical/appBar/index.tsx
@@ -1,21 +1,18 @@
-// ** React Imports
-import React, {ReactNode} from 'react'
+import React, {ReactNode} from 'react';
 
-// ** MUI Imports
-import {styled} from '@mui/material/styles'
-import MuiAppBar, {AppBarProps} from '@mui/material/AppBar'
-import MuiToolbar, {ToolbarProps} from '@mui/material/Toolbar'
+import { styled } from '@mui/material/styles';
+import MuiAppBar, { AppBarProps } from '@mui/material/AppBar';
+import MuiToolbar, { ToolbarProps } from '@mui/material/Toolbar';
 
-// ** Type Import
-import {Settings} from 'src/@core/context/settingsContext'
-import VerticalNavHeader from "../navigation/VerticalNavHeader";
+import { Settings } from 'src/@core/context/settingsContext';
+import VerticalNavHeader from '../navigation/VerticalNavHeader';
 
 interface Props {
-  hidden: boolean
-  settings: Settings
-  toggleNavVisibility: () => void
-  saveSettings: (values: Settings) => void
-  verticalAppBarContent?: (props?: any) => ReactNode
+  hidden: boolean;
+  settings: Settings;
+  toggleNavVisibility: () => void;
+  saveSettings: (values: Settings) => void;
+  verticalAppBarContent?: (props?: any) => ReactNode;
 }
 
 const AppBar = styled(MuiAppBar)<AppBarProps>(({ theme }) => ({
@@ -26,14 +23,15 @@ const AppBar = styled(MuiAppBar)<AppBarProps>(({ theme }) => ({
   padding: theme.spacing(0, 6),
   color: theme.palette.text.primary,
   minHeight: theme.mixins.toolbar.minHeight,
-  [theme.breakpoints.down('sm')]: {
-    paddingLeft: theme.spacing(4),
-    paddingRight: theme.spacing(4)
-  },
-  zIndex : 1400
-}))
+  zIndex: 1400,
+  display: 'flex',
+  flexDirection: 'row'
+}));
 
 const Toolbar = styled(MuiToolbar)<ToolbarProps>(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
   width: '100%',
   borderBottomLeftRadius: 10,
   borderBottomRightRadius: 10,
@@ -41,20 +39,26 @@ const Toolbar = styled(MuiToolbar)<ToolbarProps>(({ theme }) => ({
   minHeight: `${theme.mixins.toolbar.minHeight}px !important`,
   transition:
     'padding .25s ease-in-out, box-shadow .25s ease-in-out, backdrop-filter .25s ease-in-out, background-color .25s ease-in-out'
-}))
+}));
 
 const LayoutAppBar = (props: Props) => {
   // ** Props
-  const { verticalAppBarContent: userVerticalAppBarContent } = props
+  const { verticalAppBarContent: userVerticalAppBarContent, hidden } = props;
 
+  // @ts-ignore
   return (
     <AppBar elevation={0} color='default' className='layout-navbar' position='static'>
       <Toolbar>
-        <VerticalNavHeader {...props} />
+        {hidden ? null :(
+          <VerticalNavHeader
+            {...props}
+            hidden={hidden}
+          />
+        )}
         {(userVerticalAppBarContent && userVerticalAppBarContent(props)) || null}
       </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default LayoutAppBar
+export default LayoutAppBar;
